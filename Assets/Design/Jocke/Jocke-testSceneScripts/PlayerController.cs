eda00a72afd4e0de409386443afaa74b0b953f5e
Joakimblom <joakim.blomstrand@student.forsbergsskola.se>
2022-06-09 14:22:11 +0200

    Made simple character controller script for testing and prototyping

--- Assets/Design/Jocke/Jocke-testSceneScripts/PlayerController.cs
+++ Assets/Design/Jocke/Jocke-testSceneScripts/PlayerController.cs
@@ -4,13 +4,30 @@ public class PlayerController : MonoBehaviour
 {
     [SerializeField] CharacterController controller;
     [SerializeField] float speed = 6f;
-
+    [SerializeField] float jumpHeight = 10f;
+    [SerializeField] float gravity = -20;
+    [SerializeField] Transform groundCheck;
+    public LayerMask groundLayer;
     private Vector3 direction;
 
     void Update()
     {
+
         float Hinput = Input.GetAxis("Horizontal");
         direction.x = Hinput * speed;
+        bool isGrounded = Physics.CheckSphere(groundCheck.position, 0.15f, groundLayer);
         controller.Move(direction * Time.deltaTime);
+
+        if (isGrounded)
+        {
+
+            if (Input.GetButtonDown("Jump"))
+          {
+             direction.y = jumpHeight;
+          }
+
+        }
+        direction.y += gravity * Time.deltaTime;
+        
     }
 }
