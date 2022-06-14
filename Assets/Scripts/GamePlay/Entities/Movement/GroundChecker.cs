9ce3e103e081a8933f4e85ff91da504f9cf5a0ea
HarryTheHeath <harry.heath@student.forsbergsskola.se>
2022-06-14 12:30:02 +0200

    set isGrounded public for testing

--- Assets/Scripts/GamePlay/Entities/Movement/GroundChecker.cs
+++ Assets/Scripts/GamePlay/Entities/Movement/GroundChecker.cs
@@ -4,7 +4,7 @@ namespace GamePlay.Entities.Movement
 {
     public class GroundChecker : MonoBehaviour
     {
-        public bool IsGrounded { get; private set; }
+        public bool IsGrounded;
         [SerializeField] private float _groundCheckLength = 1f;
         [SerializeField] private float _groundCheckRadius = 0.5f;
         [SerializeField] private LayerMask _groundLayers;
@@ -14,13 +14,17 @@ namespace GamePlay.Entities.Movement
             CheckIfGrounded();
         }
 
+        
         private void CheckIfGrounded()
         {
-            var ray = new Ray(transform.position, Vector3.down);
+            var pos = transform.position;
+            var ray = new Ray(pos, Vector3.down);
+                
             IsGrounded = Physics.SphereCast(ray, _groundCheckRadius, _groundCheckLength, _groundLayers);
-            // Debug.DrawRay(transform.position, Vector3.down * groundCheckLength, Color.magenta);
+            Debug.DrawRay(pos, Vector3.down * _groundCheckLength, Color.magenta);
         }
 
+        
         private void OnDrawGizmos()
         {
             Gizmos.DrawSphere(transform.position + Vector3.down * _groundCheckLength, _groundCheckRadius);
