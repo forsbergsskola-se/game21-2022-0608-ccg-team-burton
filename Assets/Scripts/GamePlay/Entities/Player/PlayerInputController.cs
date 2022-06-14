c2db8c656dd0b222b6a01b6da1b80a8ac9915bdc
HarryTheHeath <harry.heath@student.forsbergsskola.se>
2022-06-14 11:03:20 +0200

    Added new script: PlayerInputController.cs

---
+++ Assets/Scripts/GamePlay/Entities/Player/PlayerInputController.cs
@@ -0,0 +1,78 @@
+using UnityEngine;
+
+namespace GamePlay.Entities.Player
+{
+    public class PlayerInputController : MonoBehaviour
+    {
+        private CommandContainer _commandContainer;
+        
+        public float WalkInput { get; private set; }
+        public bool WalkLeftInput { get; private set; }
+        public bool WalkRightInput { get; private set; }
+
+        public bool JumpInputDown { get; private set; }
+        public bool JumpInputUp { get; private set; }
+        public bool JumpInput { get; private set; }
+        private bool _noInput;
+
+        
+        private void Awake()
+        {
+            _commandContainer = GetComponent<CommandContainer>();
+        }
+
+        
+        private void Update()
+        {
+            HandleInput();
+            SetCommands();
+        }
+        
+        
+        private void HandleInput()
+        {
+            WalkInput = Input.GetAxisRaw("Horizontal");
+
+            switch (WalkRightInput)
+            {
+                case true when WalkLeftInput: // pressing both keeps player still
+                    WalkInput = 0;
+                    _noInput = false;
+                    break;
+                
+                case true:
+                    WalkInput = 1;
+                    _noInput = false;
+                    break;
+                
+                default:
+                {
+                    if (WalkLeftInput)
+                    {
+                        WalkInput = -1;
+                        _noInput = false;
+                    }
+                    
+                    else
+                        _noInput = true;
+                    break;
+                }
+            }
+
+            JumpInputDown = Input.GetKeyDown(KeyCode.Space);
+            JumpInputUp = Input.GetKeyUp(KeyCode.Space);
+            JumpInput = Input.GetKey(KeyCode.Space);
+        }
+
+        
+        private void SetCommands()
+        {
+            _commandContainer.walkCommand = WalkInput;
+            _commandContainer.walkLeftCommand = WalkLeftInput;
+            _commandContainer.walkRightCommand = WalkRightInput;
+            _commandContainer.JumpCommandDown = JumpInputDown;
+            _commandContainer.JumpCommandUp = JumpInputUp;
+            _commandContainer.JumpCommand = JumpInput;
+        }
+    }
+}
