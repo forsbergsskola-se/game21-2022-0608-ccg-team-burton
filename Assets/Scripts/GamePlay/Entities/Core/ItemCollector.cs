76286f469e1297a148a7712cb88509267e3197bd
Pavel Sarmiento <pavelsarmiento@hotmail.com>
2022-06-15 15:52:17 +0200

    ADD: Small coin counter on pickup and adjustments to scene

--- Assets/Scripts/GamePlay/Entities/Core/ItemCollector.cs
+++ Assets/Scripts/GamePlay/Entities/Core/ItemCollector.cs
@@ -1,17 +1,20 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class ItemCollector : MonoBehaviour{
 
+    [SerializeField] TextMeshProUGUI cointText;
+    
     int coins = 0;
     
     void OnTriggerEnter2D(Collider2D col){
         if (col.gameObject.CompareTag("Coin")){
             Destroy(col.gameObject);
             coins++;
-            Debug.Log("Coins: " + coins);
+            cointText.text = "Coins: " + coins;
         }
     }
 }
