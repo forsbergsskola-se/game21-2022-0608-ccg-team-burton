9f4b5c23954d1f4c87d5d808db0f976262348c14
Jonathan Jonsson <jonsson.h.jonathan@gmail.com>
2022-06-15 13:06:05 +0200

    Renaming and tuning knockback for hazard
    Some extra field implementation

---
+++ Assets/Scripts/GamePlay/Entities/Core/DamageOnContact.cs
@@ -0,0 +1,29 @@
+using Entity;
+using UnityEngine;
+
+
+namespace Entity.Hazard
+{
+ 
+    /// <summary>
+    /// Temporary script for damaging the player. Should be modified or removed.
+    /// </summary>
+    public class DamageOnContact : MonoBehaviour
+    {
+        [SerializeField]
+        private int _damage = 1;
+
+        [SerializeField] private float _knockbackMultiplier=1;
+        private void OnCollisionEnter2D(Collision2D col)
+        {
+            if (col.gameObject.CompareTag("Player"))
+            {
+                Debug.Log($"{name} dealt damage to {col.gameObject.name}");
+                col.gameObject.GetComponent<Knockback>().DoKnockBack(col.gameObject.GetComponent<Rigidbody2D>(),
+                    -col.GetContact(0).normal, _knockbackMultiplier);
+                // Debug.DrawRay(col.GetContact(0).point, -col.GetContact(0).normal*100, Random.ColorHSV(0f, 1f, 1f, 1f, 0.5f, 1f), 10f );
+                col.gameObject.GetComponent<IDamageable>().ModifyHealth(-_damage);
+            }
+        }
+    }   
+}
