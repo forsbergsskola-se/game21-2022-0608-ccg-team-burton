0e4bdaded7b456e2ab0871a38875c2149cfec9ce
HarryTheHeath <harry.heath@student.forsbergsskola.se>
2022-06-15 19:10:59 +0200

    cleaned-up inputs + made jump only triggered by GetKeyDown

--- Assets/Scripts/GamePlay/Entities/Player/PlayerInputController.cs
+++ Assets/Scripts/GamePlay/Entities/Player/PlayerInputController.cs
@@ -5,15 +5,10 @@ namespace GamePlay.Entities.Player
     public class PlayerInputController : MonoBehaviour
     {
         private CommandContainer _commandContainer;
-        
         public float WalkInput { get; private set; }
-        public bool WalkLeftInput { get; private set; }
-        public bool WalkRightInput { get; private set; }
-
-        public bool JumpInputDown { get; private set; }
-        public bool JumpInputUp { get; private set; }
+        //public bool JumpInputDown { get; private set; }
+        //public bool JumpInputUp { get; private set; }
         public bool JumpInput { get; private set; }
-        private bool _noInput;
 
         
         private void Awake()
@@ -32,19 +27,15 @@ namespace GamePlay.Entities.Player
         private void HandleInput()
         {
             WalkInput = Input.GetAxis("Horizontal");
-            JumpInputDown = Input.GetKeyDown(KeyCode.Space);
-            JumpInputUp = Input.GetKeyUp(KeyCode.Space);
-            JumpInput = Input.GetKey(KeyCode.Space);
+            //JumpInputDown = Input.GetKeyDown(KeyCode.Space);
+            //JumpInputUp = Input.GetKeyUp(KeyCode.Space);
+            JumpInput = Input.GetKeyDown(KeyCode.Space);
         }
 
         
         private void SetCommands()
         {
             _commandContainer.WalkCommand = WalkInput;
-            _commandContainer.WalkLeftCommand = WalkLeftInput;
-            _commandContainer.WalkRightCommand = WalkRightInput;
-            _commandContainer.JumpCommandDown = JumpInputDown;
-            _commandContainer.JumpCommandUp = JumpInputUp;
             _commandContainer.JumpCommand = JumpInput;
         }
     }
