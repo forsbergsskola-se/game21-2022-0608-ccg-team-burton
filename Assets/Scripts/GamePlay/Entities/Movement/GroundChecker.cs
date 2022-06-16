00e0b8df5a00069596e9f1a192302dc01ed9ae7c
HarryTheHeath <harry.heath@student.forsbergsskola.se>
2022-06-16 16:58:13 +0200

    added roof check

--- Assets/Scripts/GamePlay/Entities/Movement/GroundChecker.cs
+++ Assets/Scripts/GamePlay/Entities/Movement/GroundChecker.cs
@@ -5,14 +5,29 @@ namespace GamePlay.Entities.Movement
     public class GroundChecker : MonoBehaviour
     {
         public bool IsGrounded;
-        [SerializeField] private float _groundCheckLength = 1f;
-        [SerializeField] private float _groundCheckRadius = 1f;
+        [SerializeField] private float _groundCheckLength = 0.53f;
+        [SerializeField] private float _groundCheckRadius = 0.53f;
+        
+        public bool IsRoofed;
+        [SerializeField] private float _roofCheckLength = 0.53f;
+        [SerializeField] private float _roofCheckRadius = 0.53f;
         [SerializeField] private LayerMask _groundLayers;
 
         private void Update()
         {
             CheckIfGrounded();
+            CheckIfRoofed();
         }
+        
+        private void CheckIfRoofed()
+        {
+            var pos = transform.position;
+            var ray = new Vector2(pos.x, pos.y);
+
+            IsRoofed = Physics2D.CircleCast(ray, _roofCheckRadius, Vector2.up, _roofCheckLength, _groundLayers);
+            Debug.DrawRay(pos, Vector3.up * _roofCheckLength, Color.red);
+        }
+        
 
         
         private void CheckIfGrounded()
@@ -27,7 +42,10 @@ namespace GamePlay.Entities.Movement
         
         private void OnDrawGizmos()
         {
+            Gizmos.color = Color.cyan;
             Gizmos.DrawSphere(transform.position + Vector3.down * _groundCheckLength, _groundCheckRadius);
+            Gizmos.color = Color.yellow;
+            Gizmos.DrawSphere(transform.position + Vector3.up * _roofCheckLength, _roofCheckRadius);
         }
     }
 }
