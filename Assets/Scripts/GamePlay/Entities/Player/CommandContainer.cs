b74fa7dd5925a39159371e58c4096a305944fa1a
MaximThellenberg <maxim.thellenberg@student.forsbergsskola.se>
2022-06-16 11:33:02 +0200

    Updateing Movement scripts

--- Assets/Scripts/GamePlay/Entities/Player/CommandContainer.cs
+++ Assets/Scripts/GamePlay/Entities/Player/CommandContainer.cs
@@ -7,12 +7,9 @@ namespace GamePlay.Entities.Player
         // these fields are visible in the inspector, which can be useful for testing.
         // But in some cases we might want to use {HideInInspector} or getters/setters to hide these fields
 
-        public float walkCommand;
-        public bool walkLeftCommand;
-        public bool walkRightCommand;
-        public bool JumpCommandDown;
-        public bool JumpCommandUp;
+        public float WalkCommand;
         public bool JumpCommand;
-        public bool AttackCommand;
+        //public bool AttackCommand;
     }
 }
+
