e39cceb22fdacff35744f9bac831f0cc71d8322c
MaximThellenberg <maxim.thellenberg@student.forsbergsskola.se>
2022-06-16 11:33:18 +0200

    Updateing movemnt Script

--- Assets/Scripts/GamePlay/Entities/Player/PlayerInputController.cs
+++ Assets/Scripts/GamePlay/Entities/Player/PlayerInputController.cs
@@ -1,3 +1,4 @@
+
 using UnityEngine;
 
 namespace GamePlay.Entities.Player
@@ -5,15 +6,10 @@ namespace GamePlay.Entities.Player
     public class PlayerInputController : MonoBehaviour
     {
         private CommandContainer _commandContainer;
-        
         public float WalkInput { get; private set; }
-        public bool WalkLeftInput { get; private set; }
-        public bool WalkRightInput { get; private set; }
-
-        public bool JumpInputDown { get; private set; }
-        public bool JumpInputUp { get; private set; }
+        //public bool JumpInputDown { get; private set; }
+        //public bool JumpInputUp { get; private set; }
         public bool JumpInput { get; private set; }
-        private bool _noInput;
 
         
         private void Awake()
@@ -32,51 +28,15 @@ namespace GamePlay.Entities.Player
         private void HandleInput()
         {
             WalkInput = Input.GetAxis("Horizontal");
-            
-            // switch (WalkRightInput)
-            // {
-            //     case true when WalkLeftInput: // pressing both keeps player still
-            //         WalkInput = 0;
-            //         _noInput = false;
-            //         break;
-            //     
-            //     case true:
-            //         WalkInput = 1;
-            //         _noInput = false;
-            //         break;
-            //     
-            //     default:
-            //     {
-            //         if (WalkLeftInput)
-            //         {
-            //             WalkInput = -1;
-            //             _noInput = false;
-            //         }
-            //         
-            //         else
-            //             _noInput = true;
-            //         break;
-            //     }
-            // }
-
-            void OnPointerDown()
-            {
-                WalkInput = Input.GetAxis("Horizontal");
-            }
-
-            JumpInputDown = Input.GetKeyDown(KeyCode.Space);
-            JumpInputUp = Input.GetKeyUp(KeyCode.Space);
-            JumpInput = Input.GetKey(KeyCode.Space);
+            //JumpInputDown = Input.GetKeyDown(KeyCode.Space);
+            //JumpInputUp = Input.GetKeyUp(KeyCode.Space);
+            JumpInput = Input.GetKeyDown(KeyCode.Space);
         }
 
         
         private void SetCommands()
         {
-            _commandContainer.walkCommand = WalkInput;
-            _commandContainer.walkLeftCommand = WalkLeftInput;
-            _commandContainer.walkRightCommand = WalkRightInput;
-            _commandContainer.JumpCommandDown = JumpInputDown;
-            _commandContainer.JumpCommandUp = JumpInputUp;
+            _commandContainer.WalkCommand = WalkInput;
             _commandContainer.JumpCommand = JumpInput;
         }
     }
