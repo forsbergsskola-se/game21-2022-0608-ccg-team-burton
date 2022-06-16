9fc1587b7c4fe6eec5aab53eef9a2379bcf554d6
MaximThellenberg <maxim.thellenberg@student.forsbergsskola.se>
2022-06-16 11:36:13 +0200

    Movment Script Update

--- Assets/Scripts/GamePlay/Entities/Movement/MovementManager.cs
+++ Assets/Scripts/GamePlay/Entities/Movement/MovementManager.cs
@@ -7,62 +7,100 @@ namespace GamePlay.Entities.Movement
     {
         private Animator _animator;
         private SpriteRenderer _renderer;
-        private Collider2D _collider;
         private Rigidbody2D _rb;
         private CommandContainer _commandContainer;
         private GroundChecker _groundChecker;
 
-        public float baseMovementSpeed;
-        public float airborneMovementSpeed;
-        public float chargingJumpSpeed;
-        public float jumpForce;
+        #region Walking;
+        [Header("WALKING")]
+        private float _baseMovementSpeed;
+        public float Acceleration = 80f;
+        public float DeAcceleration = 55f;
+        public float WalkClamp = 12f;
+        #endregion
+
+        #region Jumping;
+
+        [Header("JUMPING")] 
+        public float BaseJumpSpeed;
+        private float _jumpHeight;
+        public bool Jumping;
+
+
+        private Vector3 _lastPos;
+        public Vector3 _jumpVelocity { get; private set; }
+        #endregion
         
+
 
         private void Awake()
         {
             //_animator = gameObject.GetComponent<Animator>();
             _renderer = GetComponent<SpriteRenderer>();
-            _collider = GetComponent<CapsuleCollider2D>();
             _commandContainer = GetComponent<CommandContainer>();
             _groundChecker = GetComponent<GroundChecker>();
             _rb = GetComponent<Rigidbody2D>();
+            Acceleration *= 100f;
+            DeAcceleration *= 100f;
         }
 
-        
+
         private void Update()
         {
-            HandleWalking();
+            CalculateJumpVelocity();
             HandleJumping();
+            HandleWalking();
+        }
+
+        private void CalculateJumpVelocity()
+        {
+            var pos = transform.position;
+            _jumpVelocity = (pos - _lastPos) / Time.deltaTime;
+            _lastPos = pos;
         }
 
 
         private void HandleJumping()
         {
+
             if (!_commandContainer.JumpCommand) return;
-            if (!_groundChecker.IsGrounded) return;
-            
-            _rb.AddForce(Vector2.up * jumpForce);
+            if (_groundChecker.IsGrounded)
+            {
+                _jumpHeight = BaseJumpSpeed * Time.deltaTime;
+                Jumping = true;
+                _rb.velocity = new Vector2(_baseMovementSpeed, _jumpHeight);
+            }
+            else
+            {
+                _jumpHeight = _baseMovementSpeed;
+                Jumping = false;
+            }
         }
 
 
         private void HandleWalking()
         {
-            var movementSpeed = 0f;
-            if (_groundChecker.IsGrounded)
-                movementSpeed = baseMovementSpeed;
+            _renderer.flipX = _commandContainer.WalkCommand switch
+            {
+                > 0 => false, 
+                < 0 => true,
+                _ => _renderer.flipX
+            };
 
-            /*if (_groundChecker.IsGrounded && _commandContainer.JumpCommand)
-                movementSpeed = chargingJumpSpeed;*/
+            if (_commandContainer.WalkCommand != 0)
+            {
+                // set walk speed
+                _baseMovementSpeed = _commandContainer.WalkCommand * Acceleration * Time.deltaTime;
+                // clamp walk speed
+                _baseMovementSpeed = Mathf.Clamp(_baseMovementSpeed, -WalkClamp, WalkClamp);
+            }
 
-            else if (!_groundChecker.IsGrounded)
-                movementSpeed = airborneMovementSpeed;
-
-            _rb.velocity = new Vector3(_commandContainer.walkCommand * movementSpeed, _rb.velocity.y, 0);
+            else
+                _baseMovementSpeed = Mathf.MoveTowards // slow down when no input
+                    (_baseMovementSpeed, 0, DeAcceleration * Time.deltaTime);
+            
+            _rb.velocity = new Vector2(_baseMovementSpeed, _rb.velocity.y);
         }
     }
-    
-    
-    
-    
 }
 
