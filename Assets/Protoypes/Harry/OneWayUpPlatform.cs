17d3bb86720f63427646aee727a78c906806660d
HarryTheHeath <harry.heath@student.forsbergsskola.se>
2022-06-22 12:20:09 +0200

    clean-up

--- Assets/Protoypes/Harry/OneWayUpPlatform.cs
+++ Assets/Protoypes/Harry/OneWayUpPlatform.cs
@@ -11,26 +11,37 @@ public class OneWayUpPlatform : MonoBehaviour
 
    private void Awake() => _collider = GetComponentInParent<BoxCollider2D>();
 
+   
+   
    private void Update()
    {
-      if (_player == null) return;
-      if (_player._currentVerticalSpeed < 0)
-         _unlockBuffer = Time.time + UnlockTime;
-      
-      
-      _collider.enabled = _player._currentVerticalSpeed <= 0 && Time.time >= _unlockBuffer;
+      if (_player == null) return; // only working otherwise
+      CheckToLetPlayerThrough(); 
    }
 
+   
+   
+   private void CheckToLetPlayerThrough()
+   {
+      if (_player._currentVerticalSpeed < 0)
+         _unlockBuffer = Time.time + UnlockTime; // time boxCollider will be disabled
+      
+      _collider.enabled = _player._currentVerticalSpeed <= 0 && Time.time >= _unlockBuffer; // disable collider until condition is met
+   }
 
+   
+   
    private void OnTriggerEnter2D(Collider2D other)
    {
-      if (other.gameObject.TryGetComponent(out NewMovement player))
-         _player = player;
+      if (other.gameObject.TryGetComponent(out NewMovement player)){}
+         _player = player; // override null check
    }
 
+   
+   
    private void OnTriggerExit2D(Collider2D other)
    {
       if (other.gameObject.TryGetComponent(out NewMovement player))
-         _player = null;
+         _player = null; // reset null check
    }
 }
