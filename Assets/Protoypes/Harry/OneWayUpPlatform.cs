d4ab73bac88fe7752277390ae76b5c040abd739e
HarryTheHeath <harry.heath@student.forsbergsskola.se>
2022-06-22 11:11:15 +0200

    working version of one-way platform logic

---
+++ Assets/Protoypes/Harry/OneWayUpPlatform.cs
@@ -0,0 +1,36 @@
+using Protoypes.Harry;
+using UnityEngine;
+
+public class OneWayUpPlatform : MonoBehaviour
+{
+   private BoxCollider2D _collider;
+   private NewMovement _player;
+   public float UnlockTime;
+   private float _unlockBuffer = float.MinValue;
+
+
+   private void Awake() => _collider = GetComponentInParent<BoxCollider2D>();
+
+   private void Update()
+   {
+      if (_player == null) return;
+      if (_player._currentVerticalSpeed < 0)
+         _unlockBuffer = Time.time + UnlockTime;
+      
+      
+      _collider.enabled = _player._currentVerticalSpeed <= 0 && Time.time >= _unlockBuffer;
+   }
+
+
+   private void OnTriggerEnter2D(Collider2D other)
+   {
+      if (other.gameObject.TryGetComponent(out NewMovement player))
+         _player = player;
+   }
+
+   private void OnTriggerExit2D(Collider2D other)
+   {
+      if (other.gameObject.TryGetComponent(out NewMovement player))
+         _player = null;
+   }
+}
