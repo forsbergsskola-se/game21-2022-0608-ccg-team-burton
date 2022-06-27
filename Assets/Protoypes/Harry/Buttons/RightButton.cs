67b7da2ff6f87744d05701eb237c389a3d91b3e6
HarryTheHeath <harry.heath@student.forsbergsskola.se>
2022-06-27 15:33:51 +0200

    fixed right movement button input

--- Assets/Protoypes/Harry/Buttons/RightButton.cs
+++ Assets/Protoypes/Harry/Buttons/RightButton.cs
@@ -4,26 +4,22 @@ using UnityEngine;
 public class RightButton : MonoBehaviour
 {
     private PlayerInputController _playerInputController;
-
     
     private void Awake()
     {
         _playerInputController = FindObjectOfType<PlayerInputController>();
     }
 
-
+    public void OnPointerDownRight()
+    {
+        _playerInputController.WalkRightDownInput = true;
+        _playerInputController.WalkRightUpInput = false;
+    }
 
     public void OnPointerUpRight()
     {
         _playerInputController.WalkRightUpInput = true;
-        _playerInputController.WalkRightDownInput = true;
-    }
-    
-    
-    
-    public void OnPointerDownRight()
-    {
         _playerInputController.WalkRightDownInput = false;
-        _playerInputController.WalkRightUpInput = false;
     }
+
 }
