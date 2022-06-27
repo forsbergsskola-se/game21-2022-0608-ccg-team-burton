cdea65f73538ac2ae406c026fe0571d79f415d60
HarryTheHeath <harry.heath@student.forsbergsskola.se>
2022-06-27 15:36:44 +0200

    progress with button movement controls

--- Assets/Scripts/GamePlay/Entities/Player/PlayerInputController.cs
+++ Assets/Scripts/GamePlay/Entities/Player/PlayerInputController.cs
@@ -4,21 +4,26 @@ namespace GamePlay.Entities.Player
     public class PlayerInputController : MonoBehaviour
     {
         private CommandContainer _commandContainer;
-        public float WalkInput { get; private set; }
-        [HideInInspector] public bool WalkLeftDownInput;
+        public float WalkInput;
+        public bool WalkLeftDownInput;
         [HideInInspector] public bool WalkLeftUpInput;
-        [HideInInspector] public bool WalkRightDownInput;
+        public bool WalkRightDownInput;
         [HideInInspector] public bool WalkRightUpInput;
-        [HideInInspector] public bool NoWalkInput;
+        public bool NoWalkInput;
         
         public bool JumpDownInput;
         public bool JumpUpInput;
+
+        public AnimationCurve WalkingCurve;
+        public float CurrentWalkSpeed;
+        public float InputAcceleration;
+        public float InputDeceleration;
         
         
         public bool AttackDownInput;
         [HideInInspector] public bool AttackUpInput;
 
-
+        
         
         private void Awake() => _commandContainer = GetComponent<CommandContainer>();
 
@@ -26,24 +31,38 @@ namespace GamePlay.Entities.Player
         
         private void Update()
         {
-            HandleInput(); // collect player inputs
+            HandleHorizontalMovement(); // collect player inputs
             SetCommands(); // assign inputs to commands
 
-            if (WalkLeftDownInput && WalkRightDownInput)
+            if (WalkLeftDownInput || WalkRightDownInput)
                 NoWalkInput = false;
 
-            if (NoWalkInput) return;
-            if (!WalkLeftDownInput && !WalkRightDownInput)
+            else if (WalkLeftUpInput && WalkRightUpInput)
                 NoWalkInput = true;
         }
         
         
+
+        private void HandleHorizontalMovement()
+        {
+            if (NoWalkInput && CurrentWalkSpeed != 0 || WalkRightDownInput && WalkLeftDownInput)
+                CurrentWalkSpeed = Mathf.MoveTowards(CurrentWalkSpeed, 0, InputDeceleration * Time.deltaTime);
+
+
+            if (WalkRightDownInput)
+                CurrentWalkSpeed += Time.deltaTime * InputAcceleration;
+
+            if (WalkLeftDownInput)
+                CurrentWalkSpeed -= Time.deltaTime * InputAcceleration;
+
+
+
+            //Mathf.Clamp(CurrentWalkSpeed, -1, 1);
+            WalkInput = WalkingCurve.Evaluate(CurrentWalkSpeed);
+        }
+
+
         
-        // left and right
-        private void HandleInput() => WalkInput = Input.GetAxis("Horizontal"); 
-
-
-
         private void SetCommands()
         {
             _commandContainer.WalkCommand = WalkInput;
