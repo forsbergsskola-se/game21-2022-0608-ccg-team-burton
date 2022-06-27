e5137d461cedb28f05c21abe9b7c7ab1b903d61d
HarryTheHeath <harry.heath@student.forsbergsskola.se>
2022-06-27 21:58:44 +0200

    player now moves smoothly with buttons :)

--- Assets/Scripts/GamePlay/Entities/Player/PlayerInputController.cs
+++ Assets/Scripts/GamePlay/Entities/Player/PlayerInputController.cs
@@ -6,18 +6,16 @@ namespace GamePlay.Entities.Player
         private CommandContainer _commandContainer;
         public float WalkInput;
         public bool WalkLeftDownInput;
-        [HideInInspector] public bool WalkLeftUpInput;
         public bool WalkRightDownInput;
-        [HideInInspector] public bool WalkRightUpInput;
         public bool NoWalkInput;
         
         public bool JumpDownInput;
         public bool JumpUpInput;
 
-        public AnimationCurve WalkingCurve;
+        //public AnimationCurve WalkingCurve;
         public float CurrentWalkSpeed;
         public float InputAcceleration;
-        public float InputDeceleration;
+        //public float InputDeceleration;
         
         
         public bool AttackDownInput;
@@ -31,34 +29,39 @@ namespace GamePlay.Entities.Player
         
         private void Update()
         {
-            HandleHorizontalMovement(); // collect player inputs
+            GatherHorizontalMovement(); // collect player inputs
+            SetHorizontalMovment(); // assign value between -1 & 1
             SetCommands(); // assign inputs to commands
 
             if (WalkLeftDownInput || WalkRightDownInput)
                 NoWalkInput = false;
 
-            else if (WalkLeftUpInput && WalkRightUpInput)
+            else if (!WalkLeftDownInput && !WalkRightDownInput)
                 NoWalkInput = true;
         }
         
         
-
-        private void HandleHorizontalMovement()
+        
+        private void GatherHorizontalMovement()
         {
-            if (NoWalkInput && CurrentWalkSpeed != 0 || WalkRightDownInput && WalkLeftDownInput)
-                CurrentWalkSpeed = Mathf.MoveTowards(CurrentWalkSpeed, 0, InputDeceleration * Time.deltaTime);
-
-
+            if (NoWalkInput || WalkLeftDownInput && WalkRightDownInput)
+                CurrentWalkSpeed = 0;
+                //Mathf.MoveTowards(CurrentWalkSpeed, 0, InputDeceleration * Time.deltaTime);
+            
             if (WalkRightDownInput)
                 CurrentWalkSpeed += Time.deltaTime * InputAcceleration;
 
             if (WalkLeftDownInput)
                 CurrentWalkSpeed -= Time.deltaTime * InputAcceleration;
+        }
 
-
-
-            //Mathf.Clamp(CurrentWalkSpeed, -1, 1);
-            WalkInput = WalkingCurve.Evaluate(CurrentWalkSpeed);
+        
+        
+        
+        private void SetHorizontalMovment()
+        {
+            CurrentWalkSpeed = Mathf.Clamp(CurrentWalkSpeed, -1, 1);
+            WalkInput = CurrentWalkSpeed;
         }
 
 
