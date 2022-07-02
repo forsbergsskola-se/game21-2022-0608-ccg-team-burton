f39811a034633af17cf53fb08a17673f769545e5
Jonathan Jonsson <jonsson.h.jonathan@gmail.com>
2022-07-02 23:43:17 +0200

    Created lootbox items, clean up, generic pickup and pickupspawner

--- Assets/Scripts/Metagame/Gacha/LootBoxController.cs
+++ Assets/Scripts/Metagame/Gacha/LootBoxController.cs
@@ -17,21 +17,24 @@ public class LootBoxController : MonoBehaviour
 
    [SerializeField] private Animator _animator;
    [SerializeField] private GameObject _itemInfoUI;
-
+   
    public Action<GameObject,InventoryItem> OnUpdateItemUI;
  
    private Item item;
    public GameObject[] ItemUIGameobjects;
 
-   // Since we are looping, we can use a list
+   // Since we need to update all elements, we can use a list
    private List<InventoryItem> gainedItems = new();
 
    // [SerializeField] private PickupSpawner[] _pickup;
 
+   private bool _boxOpenedCurrentSession = false;
+   
    public GameObject DroppedItem;   
 
    private void Start()
    {
+      _boxOpenedCurrentSession = false;
       foreach (var itemElement in ItemUIGameobjects)
       {
          itemElement.SetActive(false);
@@ -43,9 +46,14 @@ public class LootBoxController : MonoBehaviour
 
    private void OnMouseUp()
    {
-      Debug.Log("Opening loot box");
-      OpenBox();
-      _animator.SetBool("OpenLootBox", true);
+      if (!_boxOpenedCurrentSession)
+      {
+         Debug.Log("Opening loot box");
+         OpenBox();
+         _boxOpenedCurrentSession = true;
+         _animator.SetBool("OpenLootBox", true); 
+      }
+      
       
    }
 
@@ -56,19 +64,20 @@ public class LootBoxController : MonoBehaviour
       {
          var LootedItemSO = LootBoxSO.PickLootTable().PickItem(); //Scriptable object
          gainedItems.Add(LootedItemSO);
-         SetUpItem(LootedItemSO);
+         SetUpItemSO(LootedItemSO);
          
           //TODO: SAVE LOOTEDITEMSO TO INVENTORY HERE <3
-          Instantiate(DroppedItem, Vector2.zero, Quaternion.identity); // When instantiated, it Autocollects to inventory
+          var item = Instantiate(DroppedItem, Vector2.zero, Quaternion.identity); // When instantiated, it Autocollects to inventory
+          // item.GetComponentInChildren<Pickup>().GetComponentInChildren<SpriteRenderer>().sprite = LootedItemSO.GetIcon();
+
       }
    }
 
-   public void SetUpItem(InventoryItem item) // Call on collect button if we dont save above in open box
+   public void SetUpItemSO(InventoryItem item) // Call on collect button if we dont save above in open box
    {
 
       DroppedItem.GetComponent<PickupSpawner>().item = item;
       Debug.Log(item.name);
-   
 
    }
    
