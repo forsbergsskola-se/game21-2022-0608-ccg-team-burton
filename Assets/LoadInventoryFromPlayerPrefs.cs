c5eb695b65867bd78cc943ded51e9df1ab066bfb
Jonathan Jonsson <jonsson.h.jonathan@gmail.com>
2022-08-08 13:48:49 +0200

    Added slotmanager

--- Assets/LoadInventoryFromPlayerPrefs.cs
+++ Assets/LoadInventoryFromPlayerPrefs.cs
@@ -4,11 +4,30 @@ public class LoadInventoryFromPlayerPrefs : MonoBehaviour
 {
     [SerializeField]
     private ItemLibrary ItemLibrary;
+
+    [SerializeField]
+    private GameObject InventorySlot;
     private void Start()
     {
+        int i = 0;
+        int j = 0;
         foreach (var item in ItemLibrary.ItemLibrarySos.GemLibrary)
         {
                 Debug.Log($"{item.GetDisplayName()} in inventory with count: {PlayerPrefs.GetInt(item.GetItemID())}");
+                if (PlayerPrefs.GetInt(item.GetItemID()) > 0)
+                {
+                    var slot = Instantiate(InventorySlot, Vector3.zero + new Vector3(i * 5, j*-5, 0),
+                        Quaternion.identity);
+                    slot.transform.parent = transform;
+                    i++;
+                    if (i > 1)
+                    {
+                        i = 0;
+                        j++;
+                    }
+
+                }
+
         }
     }
 }
