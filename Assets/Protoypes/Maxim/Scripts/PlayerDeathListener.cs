feacd8b0edcd7c16b2c0d78dbf28d79182651ff3
MaximThellenberg <maxim.thellenberg@student.forsbergsskola.se>
2022-08-09 10:51:51 +0200

    Falling kills now
    Co-Authored-By: Jonathan Jonsson <jonsson.h.jonathan@gmail.com>

--- Assets/Protoypes/Maxim/Scripts/PlayerDeathListener.cs
+++ Assets/Protoypes/Maxim/Scripts/PlayerDeathListener.cs
@@ -9,7 +9,7 @@ public class PlayerDeathListener : MonoBehaviour
     [SerializeField] GameEvent _gameEvent;
     [SerializeField] UnityEvent _unityEvent;
 
-    void Awake()
+    void Start()
     {
         _gameEvent.Register(playerDeathListener:this);
         Debug.Log("Registered");
