e2770ad2851c3f1103ff9db334ec6c1a6ffa52b6
HarryTheHeath <harry.heath@student.forsbergsskola.se>
2022-08-12 12:11:58 +0200

    Update PauseMenu.cs

--- Assets/PauseMenu.cs
+++ Assets/PauseMenu.cs
@@ -1,6 +1,24 @@
+using TMPro;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
-public class PauseMenu : MonoBehaviour
+public class PauseMenu : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
+    #region Buttons;
+        
+    [Header("Ad Buttons")]
+    public TextMeshProUGUI DebugText;
+
+    #endregion
     
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        DebugText.text = $"Hovering over {name}";
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        DebugText.text = "Nothing Selected";
+    }
 }
