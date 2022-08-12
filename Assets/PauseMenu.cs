b412e69a5d2d3bfec5bc29517bf9826b731bbe71
HarryTheHeath <harry.heath@student.forsbergsskola.se>
2022-08-12 12:42:21 +0200

    Pause Menu Script can now change sprites

--- Assets/PauseMenu.cs
+++ Assets/PauseMenu.cs
@@ -1,6 +1,7 @@
 using TMPro;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class PauseMenu : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
@@ -8,17 +9,23 @@ public class PauseMenu : MonoBehaviour, IPointerEnterHandler, IPointerExitHandle
         
     [Header("Ad Buttons")]
     public TextMeshProUGUI DebugText;
-
+    public Image PauseMenuImage;
+    public Sprite PauseMenuChange;
+    private Sprite PauseMenuOriginal;
     #endregion
-    
+
+    private void Awake() => PauseMenuOriginal = PauseMenuImage.sprite;
 
     public void OnPointerEnter(PointerEventData eventData)
     {
         DebugText.text = $"Hovering over {name}";
+        PauseMenuImage.sprite = PauseMenuChange;
     }
 
     public void OnPointerExit(PointerEventData eventData)
     {
         DebugText.text = "Nothing Selected";
+        PauseMenuImage.sprite = PauseMenuOriginal;
+
     }
 }
