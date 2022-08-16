507bdc6e92cda7141f0c62fde089e7d44f2d7642
Pavel Sarmiento <pavelsarmiento@hotmail.com>
2022-08-16 10:48:44 +0200

    UPDATE: Fixed sound not working on collecting coins

--- Assets/Scripts/GamePlay/Entities/Core/ItemCollector.cs
+++ Assets/Scripts/GamePlay/Entities/Core/ItemCollector.cs
@@ -1,38 +1,40 @@
+using FMOD.Studio;
+using FMODUnity;
 using TMPro;
 using UnityEngine;
 
-public class ItemCollector : MonoBehaviour{
-
+public class ItemCollector : MonoBehaviour
+{
     [SerializeField] public TextMeshProUGUI coinText;
     [SerializeField] public int coinValue;
-    [HideInInspector] public int _coinCounter = 0;
+    [HideInInspector] public int _coinCounter;
+    public EventReference CollectCoinSoundFile;
+    EventInstance _collectCoinSound;
 
 
-    private SoundMananger _soundManager;
-    // public FMODUnity.EventReference CollectCoinSoundFile;
-    // private FMOD.Studio.EventInstance _collectCoinSound;
+    SoundMananger _soundManager;
 
-    private void Awake() => _soundManager = FindObjectOfType<SoundMananger>();
+    void Awake(){
+        _soundManager = FindObjectOfType<SoundMananger>();
+    }
 
-    private void Start()
-    {
-        // _collectCoinSound = FMODUnity.RuntimeManager.CreateInstance(CollectCoinSoundFile);
+    void Start(){
+        _collectCoinSound = RuntimeManager.CreateInstance(CollectCoinSoundFile);
         UpdateCoinText(0);
-    } 
+    }
 
 
-    
-    private void OnTriggerEnter2D(Collider2D col)
-    {
+    void OnTriggerEnter2D(Collider2D col){
         if (!col.gameObject.CompareTag("Coin")) return;
-        
+
         Destroy(col.gameObject);
-        // _soundManager.PlayStackingSound(_collectCoinSound);
-        _coinCounter +=  coinValue;
+        _soundManager.PlayStackingSound(_collectCoinSound);
+        _coinCounter += coinValue;
         UpdateCoinText(_coinCounter);
     }
 
-    
-    
-    public void UpdateCoinText(int value) => coinText.text = $"{value}";
+
+    public void UpdateCoinText(int value){
+        coinText.text = $"{value}";
+    }
 }
