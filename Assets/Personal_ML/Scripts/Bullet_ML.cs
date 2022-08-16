64450b878c7d87e29935daaa88e9ad188ca8782a
joechip125 <mattias.lonnqvist@student.forsbergsskola.se>
2022-08-16 18:52:14 +0200

    update

--- Assets/Personal_ML/Scripts/Bullet_ML.cs
+++ Assets/Personal_ML/Scripts/Bullet_ML.cs
@@ -17,45 +17,29 @@ public class Bullet_ML : MonoBehaviour
     {
         timeAlive = 0;
     }
-
-    private void OnCollisionEnter2D(Collision2D col)
-    {
-        var layer = col.transform.gameObject.layer;
-   
-        Debug.Log(col.collider.transform.name);
-        if (layer == 8)
-        {
-            Debug.Log("player hit");
-            col.gameObject.GetComponent<IDamageable>().ModifyHealth(-damageAmount);
-            gameObject.SetActive(false);
-        }
-        else
-        {
-            Debug.Log("player hit");
-        }
-    }
     
 
      private void OnTriggerEnter2D(Collider2D col)
     {
         var layer = col.transform.gameObject.layer;
-    
-        Debug.Log("trigger something");
+        
         if (layer == 8)
         {
-            Debug.Log("player hit");
-            col.gameObject.GetComponent<IDamageable>().ModifyHealth(-damageAmount);
+            Debug.Log(col.transform.name);
+            //col.gameObject.GetComponent<IDamageable>().ModifyHealth(-damageAmount);
+
+            if (col.gameObject.GetComponent<IDamageable>() == null)
+            {
+                Debug.Log("no health");
+            }
+            else
+            {
+                col.gameObject.GetComponent<IDamageable>().ModifyHealth(-damageAmount);
+                Debug.Log("some health");
+            }
             gameObject.SetActive(false);
         }
-        else if(layer == 0)
-        {
-            Debug.Log("trigger something");
-        }
-    
-        //else if(timeAlive < 1)
-        //{
-        //    //     Physics2D.IgnoreCollision(col, gameObject.GetComponent<Collider2D>());
-        //}
+      
     }
 
     void Update()
