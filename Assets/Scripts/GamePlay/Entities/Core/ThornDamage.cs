13e17dac1798dfbcfe34121bb41bbe1c26edfdcf
jackahlberg <ahlberg.jack@gmail.com>
2022-08-18 13:24:40 +0200

    Adds thorn damage script

---
+++ Assets/Scripts/GamePlay/Entities/Core/ThornDamage.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using Entity;
+using UnityEngine;
+
+public class ThornDamage : MonoBehaviour
+{
+    [SerializeField]
+    private int _damage = 1;
+
+    private float _savedTime = 0f;
+    private float _delayTime = 1f;
+    
+    private void OnTriggerStay2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            if ((Time.time - _savedTime) > _delayTime)
+            {
+                Debug.Log($"{name} dealt damage to {other.gameObject.name}");
+                other.gameObject.GetComponent<IDamageable>().ModifyHealth(-_damage);
+            }
+        }
+    }
+}
