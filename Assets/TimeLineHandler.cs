d6f2430a5a657115fa6647fe886e9c882fd5cd56
HarryTheHeath <harry.heath@student.forsbergsskola.se>
2022-08-18 15:47:54 +0200

    TimeHandler now activates buttons and timer when timeline is over

--- Assets/TimeLineHandler.cs
+++ Assets/TimeLineHandler.cs
@@ -1,5 +1,4 @@
 using System.Collections;
-using System.Collections.Generic;
 using Protoypes.Harry;
 using UnityEngine;
 
@@ -7,9 +6,27 @@ public class TimeLineHandler : MonoBehaviour
 {
     [SerializeField] private GameObject _timeLine;
     [SerializeField] private NewMovement _playerMovement;
+
+    private GameObject HUD;
+    private GameObject LeftButton;
+    private GameObject RightButton;
+    private GameObject JumpButton;
+    private GameObject AttackButton;
+    private LevelCompleted _levelCompleted;
+    
     [SerializeField] float _secondsToActivate;
 
-    void Start()
+    private void Awake()
+    {
+        HUD = FindObjectOfType<MovementButtonsHitboxManager>().gameObject;
+        LeftButton = HUD.transform.Find("Left Button").gameObject;
+        RightButton = HUD.transform.Find("Right Button").gameObject;
+        AttackButton = HUD.transform.Find("Attack Button").gameObject;
+        JumpButton = HUD.transform.Find("Jump Button").gameObject;
+        _levelCompleted = FindObjectOfType<LevelCompleted>();
+    }
+
+    private void Start()
     {
         //If timeline has run once this level
         if (PlayerPrefs.GetInt(PlayerPrefsKeys.TimeLineActive.ToString()) == 0)
@@ -21,20 +38,31 @@ public class TimeLineHandler : MonoBehaviour
 
         //If timeline has been run on this level previously
         _playerMovement.enabled = true;
-
+        ToggleTimerAndButtons(true);
     }
+    
+    
     
     private IEnumerator ActivationRoutine()
     {
         //Wait for 14 secs. 
         yield return new WaitForSeconds(_secondsToActivate);
         _playerMovement.enabled = true;
+        ToggleTimerAndButtons(true);
         PlayerPrefs.SetInt(PlayerPrefsKeys.TimeLineActive.ToString(), 1);
- 
     }
 
-    public void ResetTimelineActive()
+    
+
+    private void ToggleTimerAndButtons(bool toggle)
     {
-        PlayerPrefs.SetInt(PlayerPrefsKeys.TimeLineActive.ToString(), 0);
+        LeftButton.SetActive(toggle);
+        RightButton.SetActive(toggle);
+        JumpButton.SetActive(toggle);
+        AttackButton.SetActive(toggle);
+        _levelCompleted.PauseTimer = !toggle;
     }
+    
+    public void ResetTimelineActive() => PlayerPrefs.SetInt(PlayerPrefsKeys.TimeLineActive.ToString(), 0);
+
 }
