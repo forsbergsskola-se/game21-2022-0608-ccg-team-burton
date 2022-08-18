d321c944fd62a72db35f2f35dc2b316aeb116320
Jonathan Jonsson <jonsson.h.jonathan@gmail.com>
2022-08-18 22:31:22 +0200

    Implemented Currency exchange

---
+++ Assets/Scripts/Metagame/Currency/CurrencyExchange.cs
@@ -0,0 +1,37 @@
+using TMPro;
+using UnityEngine;
+
+public class CurrencyExchange : MonoBehaviour
+{
+    [SerializeField] private int _coinCost;
+    [SerializeField] private int _buttonsGained;
+
+    [SerializeField] private TMP_Text _coinsTextOnButton;
+    [SerializeField] private TMP_Text _buttonsRecivedText;
+    [SerializeField] private TMP_Text _coinsUIText;
+    [SerializeField] private TMP_Text _buttonsUIText;
+
+    private void Start()
+    {
+        _coinsTextOnButton.SetText(_coinCost.ToString() +" Coins");
+        _buttonsRecivedText.SetText(_buttonsGained.ToString());
+        
+    }
+
+    public void ExchangeCoinsForButtons()
+    {
+        if (PlayerPrefs.GetInt(PlayerPrefsKeys.CurrentCoins.ToString()) < _coinCost)
+            return;
+        
+        PlayerPrefs.SetInt(PlayerPrefsKeys.CurrentCoins.ToString(), PlayerPrefs.GetInt(PlayerPrefsKeys.CurrentCoins.ToString()) - _coinCost);
+        PlayerPrefs.SetInt(PlayerPrefsKeys.CurrentButtons.ToString(), PlayerPrefs.GetInt(PlayerPrefsKeys.CurrentButtons.ToString()) + _buttonsGained);
+        
+        
+        _coinsUIText.SetText(PlayerPrefs.GetInt(PlayerPrefsKeys.CurrentCoins.ToString()).ToString());
+        _buttonsUIText.SetText(PlayerPrefs.GetInt(PlayerPrefsKeys.CurrentButtons.ToString()).ToString());
+        
+        
+    }
+    
+
+}
